48524ac82c047163f2ccf59e6c3f1be6c6ed42e2
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2026-01-30 07:44:52 +0900

    参照について知ってる言語に変換。

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@
 fn main() {
     println!("Hello, world!");
 
-    let a = 1; // lock
-    let b = &a; // reference to a (not move ownership, not editable and can make many)
+    // C#: readonly int a = 1;と同じ
+    let a = 1; // 再代入不可の実行時決定型の変数
+    // C#: int b => a;と同じ
+    let b = &a; // aへの参照変数で、所有権のショートカットのようなもので複数作成可
 
-    let mut a2 = 1; // lock ()
-    let b2 = &mut a2; // reference to a2 (move ownership, editable and can make one)
-
-    *b2 = 2; // dereference and edit
+    // C++: int *a2 = 1;と同じ
+    let mut a2 = 1; // 再代入可の実行時決定型の変数
+    // C++: int *b2 = std::move(a2);と同じ
+    let b2 = &mut a2; // a2への可変変数で、所有権が完全にこの変数に移動するので複数作成不可、更にa2の存在はなくなる
+    // C++: *b2 = 2;と同じ
+    *b2 = 2; // b2の値を変更する場合は、逆参照が必要
 }
