f6427f9a7e0c4761bb701a2ee22e634786dedabf
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2026-01-29 19:28:47 +0900

    参照について学習。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// 1 line comment
+/*
+    Multi line comment
+    Multi line comment
+*/
+
 fn main() {
     println!("Hello, world!");
+
+    let a = 1; // lock
+    let b = &a; // reference to a (not move ownership, not editable and can make many)
+
+    let mut a2 = 1; // lock ()
+    let b2 = &mut a2; // reference to a2 (move ownership, editable and can make one)
+
+    *b2 = 2; // dereference and edit
 }
