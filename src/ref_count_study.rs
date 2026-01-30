974ec3ef839807f37766d13813a8e947a19330c2
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2026-01-31 03:46:51 +0900

    参照カウントを使った参照を学習。

---
+++ src/ref_count_study.rs
@@ -0,0 +1,15 @@
+use std::rc::Rc;
+
+struct BaseData {
+    key: String,
+}
+
+impl BaseData {
+    fn new(key: String) -> Rc<Self> {
+        Rc::new(Self { key }) // ヒープメモリにインスタンスを作成
+    }
+
+    fn see(dataRef: &Rc<Self>) {
+        dataRef.clone(); // 仮の所有権の作成
+    }
+}
